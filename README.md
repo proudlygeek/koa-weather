811eab60a788e2e51e7313fe79cfe2122bed6163
Gianluca Bargelli <gianluca.bargelli@immobiliare.it>
2014-06-10 17:37:09 +0200

    Added time header
    Added Redis Cache Middleware

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
+# weather
 
+An example Weather API response: given an IP it tracks down the 
+
+It uses Node.js 0.11.x Harmony Generators and Koa Framework[1].
+
+## Usage
+You will need Node 0.11.x to run the server. 
+If you want to just try it out you can quickly deploy a dokku container / heroku application.
+
+To deploy it on Heroku just:
+
+```bash
+heroku apps:create
+heroku addons:add redistogo:nano
+git push heroku master
+```
+
+Here's a sample endpoint to try it out:
+
+`http://koa-weather.herokuapp.com?ip=8.8.8.8`
+
+If you *omit* the GET parameter then the *requester's IP* is used.
+
+[1]: http://koajs.com/
--- index.js
+++ index.js
@@ -1,24 +1,54 @@
 'use strict';
 
-var koa = require('koa'),
-    Q   = require('q'),
-    app = koa();
+var koa         = require('koa'),
+    Q           = require('q'),
+    app         = koa();
 
-var request = Q.denodeify(require('request'));
-var port = process.env.PORT || 8080;
+var request     = Q.denodeify(require('request')),
+    port        = process.env.PORT || 8080;
 
-var geolocation = {
+if (process.env.REDISTOGO_URL) {
+    var rtg     = require('url').parse(process.env.REDISTOGO_URL);
+    var redis   = require('redis').createClient(rtg.port, rtg.hostname);
+
+    redis.auth(rgt.auth.split(":")[1]);
+} else {
+    var redis   = require('redis').createClient();
+}
+
+var Cache = {
+    redis: {
+        get: Q.denodeify(redis.get.bind(redis)),
+        set: Q.denodeify(redis.set.bind(redis))
+    },
+    ttl: 120,
+    get: function(key) {
+        return this.redis.get(key).then(function(value) {
+            return JSON.parse(value);
+        });
+    },
+    set: function(key, value) {
+        return this.redis.set(key, JSON.stringify(value)).then(function(value) {
+            return value;
+        });
+    },
+    expire: function(key, ttl) {
+        redis.expire(key, this.ttl || ttl);
+    },
+};
+
+var Geolocation = {
     getCityAsync: function(ip) {
-        var address = ip || '';
+        var address = (ip === "::1" || ip === undefined) ? '' : ip;
         var endpoint = 'http://geoip.smart-ip.net/json/' + address;
 
         return request(endpoint).then(function(params) {
             return JSON.parse(params[0].body);
         });
-    }
+    }   
 };
 
-var weather = {
+var Weather = {
     getForecastAsync: function(city, country) {
         var q = city + ',' + country;
         var endpoint = 'http://api.openweathermap.org/data/2.5/weather?units=metric&q=' + q;
@@ -29,20 +59,53 @@ var weather = {
     }
 };
 
+app.proxy = true;
+
+app.use(function *(next) {
+    var start = new Date;
+    yield next;
+    var end = new Date - start;
+    this.set('X-Response-Time', end + 'ms');
+});
+
+app.use(function *(next) {
+    var start = new Date;
+    yield next;
+    var ms = new Date - start;
+    console.log('[%s] %s %s - %s ms', this.ip, this.method, this.url, ms);
+});
+
+app.use(function *(next) {
+    var ip = this.query['ip'] || this.ip;
+    var cached = yield Cache.get(ip);
+
+    if (cached) {
+        var response = cached;        
+    } else {
+        var response = yield next;
+
+        yield Cache.set(ip, response);
+        Cache.expire(ip);
+    }
+
+    this.type = 'application/json; charset=utf-8';
+    this.body = response;
+});
+
 app.use(function *(){
-    var ip = this.query['ip'] || this.req.ip;
-    var city = yield geolocation.getCityAsync(ip);
-    var forecast = yield weather.getForecastAsync(city.city, city.countryCode);
+    var ip = this.query['ip'] || this.ip;
+    var city = yield Geolocation.getCityAsync(ip); 
+    var forecast = yield Weather.getForecastAsync(city.city, city.countryCode);
     var temperature = Math.round(forecast.main.temp);
 
     var response = {
+        ip: ip,
         city: city.city,
         temperature: temperature,
         message: 'Today, ' + city.city + ' will be ' + temperature + ' degrees.'
     };
 
-    this.type = 'application/json; charset=utf-8';
-    this.body = response;
+    return response;
 });
 
 app.listen(port);
