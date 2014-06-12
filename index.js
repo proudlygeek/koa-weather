b5a06fc81cda61b2f17e4a18c1bdd0bfb3ea64bc
Gianluca Bargelli <gianluca.bargelli@immobiliare.it>
2014-06-12 10:22:07 +0200

    Fixed to have auth only with redistogo addon

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ if (redisUrl) {
     var rtg     = require('url').parse(redisUrl);
     var redis   = require('redis').createClient(rtg.port, rtg.hostname);
 
-    redis.auth(rtg.auth.split(":")[1]);
+    if (process.env.REDISTOGO_URL) {
+        redis.auth(rtg.auth.split(":")[1]);
+    }
 } else {
     var redis   = require('redis').createClient();
 }
